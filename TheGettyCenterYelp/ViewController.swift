772ac353763c784f61cc75415023e9675a64cf23
Vandan Patel <vandan.ets@gmail.com>
2019-08-05 23:49:47 -0400

    Initial Commit

---
+++ TheGettyCenterYelp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheGettyCenterYelp
+//
+//  Created by Vandan Patel on 8/5/19.
+//  Copyright © 2019 Vandan Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
