edd9f4a1d5ed4f631dca126e4dae41256c9f2f91
Vandan Patel <vandan.ets@gmail.com>
2019-08-06 00:27:33 -0400

    Creates entities for business and business location.

---
+++ TheGettyCenterYelp/BusinessDetailsModule/Entity/Business.swift
@@ -0,0 +1,29 @@
+//
+//  Business.swift
+//  TheGettyCenterYelp
+//
+//  Created by Vandan Patel on 8/6/19.
+//  Copyright © 2019 Vandan Patel. All rights reserved.
+//
+
+import Foundation
+
+struct Business: Codable {
+    let name: String
+    let imageURL: String
+    let yelpURL: String
+    let phoneNumber: String
+    let rating: Double
+    let location: BusinessLocation
+    let photos: [String]
+    
+    private enum CodingKeys: String, CodingKey {
+        case name
+        case imageURL = "image_url"
+        case yelpURL = "url"
+        case phoneNumber = "display_phone"
+        case rating
+        case location
+        case photos
+    }
+}
---
+++ TheGettyCenterYelp/BusinessDetailsModule/Entity/BusinessLocation.swift
@@ -0,0 +1,29 @@
+//
+//  BusinessLocation.swift
+//  TheGettyCenterYelp
+//
+//  Created by Vandan Patel on 8/6/19.
+//  Copyright © 2019 Vandan Patel. All rights reserved.
+//
+
+import Foundation
+
+struct BusinessLocation: Codable {
+    let address1: String
+    let address2: String
+    let address3: String
+    let city: String
+    let zipcode: String
+    let state: String
+    let country: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case address1
+        case address2
+        case address3
+        case city
+        case zipcode = "zip_code"
+        case state
+        case country
+    }
+}
