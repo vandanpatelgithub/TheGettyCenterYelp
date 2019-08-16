101c69b41c691970a6e8635d21c4640d54bf403f
Vandan Patel <vandan.ets@gmail.com>
2019-08-15 23:58:21 -0400

    Creates method in NetworkManager to get reviews.

--- TheGettyCenterYelp/Network Layer/Manager/NetworkManager.swift
+++ TheGettyCenterYelp/Network Layer/Manager/NetworkManager.swift
@@ -10,9 +10,10 @@ import Foundation
 
 protocol NetworkManagable {
     func getDetailsForBusiness(withID id: String, completion: @escaping (Result<Business, Error>) -> ())
+    func getReviewsForBusiness(withID id: String, completion: @escaping (Result<BusinessReviews, Error>) -> ())
 }
 
-struct NetworkManager: NetworkManagable {    
+struct NetworkManager: NetworkManagable {
     let router: Router<YelpAPI>
     
     func getDetailsForBusiness(withID id: String, completion: @escaping (Result<Business, Error>) -> ()) {
@@ -22,6 +23,20 @@ struct NetworkManager: NetworkManagable {
                 do {
                     let business = try JSONDecoder().decode(Business.self, from: data)
                     completion(.success(business))
+                } catch {
+                    completion(.failure(error))
+                }
+            }
+        }
+    }
+    
+    func getReviewsForBusiness(withID id: String, completion: @escaping (Result<BusinessReviews, Error>) -> ()) {
+        router.request(.getReviewsForBusiness(withID: id)) { (data, response, error) in
+            if let error = error { completion(.failure(error)) }
+            else if let data = data, let _ = response {
+                do {
+                    let businessReviews = try JSONDecoder().decode(BusinessReviews.self, from: data)
+                    completion(.success(businessReviews))
                 } catch {
                     completion(.failure(error))
                 }
